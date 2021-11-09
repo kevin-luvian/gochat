362fb1a3d041250c2616609c297609ca4d1ef0a3
kevin-luvian <kevinlh90@gmail.com>
2021-11-09 07:06:03 +0700

    feat: adding test flow log

--- backend/dao/userDao_test.go
+++ backend/dao/userDao_test.go
@@ -2,22 +2,32 @@ package dao
 
 import (
 	libsql "database/sql"
-	"gochat/lib/database/sql"
+	"gochat/database"
 	"gochat/model"
 	"testing"
 
+	"github.com/joho/godotenv"
 	"github.com/sirupsen/logrus"
 )
 
-func makeTestUserDB(t *testing.T) (db *libsql.DB, cleanup func() error) {
-	logrus.Info("Creating MYSQLDB for testing")
-	defer logrus.Info("MYSQLDB created")
+func init() {
+	logrus.Info("Starting userDao test")
+	err := godotenv.Load("../.env")
 
-	testDB := sql.MakeTestSQLDB()
-	testDB.Connect()
+	if err != nil {
+		logrus.Fatal("Error loading .env file")
+	}
+}
+
+func makeTestUserDB(t *testing.T) *libsql.DB {
+	logrus.Info("Creating MYSQLDB for testing")
+
+	testDB := database.GetMYSQLDBTest()
+
+	// clean the database
 	testDB.DropCreateTables(model.User{})
 
-	return testDB.GetDatabase(), testDB.GetDatabase().Close
+	return testDB.GetDatabase()
 }
 
 func checkUserEqual(userA model.User, userB model.User) bool {
@@ -26,20 +36,20 @@ func checkUserEqual(userA model.User, userB model.User) bool {
 }
 
 func TestFindAllUser(t *testing.T) {
-	db, close := makeTestUserDB(t)
-	defer close()
+	db := makeTestUserDB(t)
 
 	userDao := MakeUserDAO(db)
 
 	// on table creation table users should be empty
 	if ok, users := userDao.FindAll(); !ok || len(users) != 0 {
 		t.Fatalf(`users is not empty. ok: %t, users: %v`, ok, users)
+	} else {
+		logrus.Info(`table user is empty`)
 	}
 }
 
 func TestCreateUser(t *testing.T) {
-	db, close := makeTestUserDB(t)
-	defer close()
+	db := makeTestUserDB(t)
 
 	userDao := MakeUserDAO(db)
 
@@ -47,83 +57,122 @@ func TestCreateUser(t *testing.T) {
 	user := model.User{Name: "Rick", Nickname: "Sanchez"}
 	if ok, _ := userDao.Create(user); !ok {
 		t.Fatalf(`can't create new user. ok: %t, user: %v`, ok, user)
+	} else {
+		logrus.Info("new user created")
 	}
 
 	// get all user, check if new user added
 	ok, users := userDao.FindAll()
 	if !ok || len(users) != 1 {
 		t.Fatalf(`database users doesn't change. ok: %t, users: %v`, ok, users)
+	} else {
+		logrus.Info("list user database increased")
 	}
 
 	// check if user is the same
 	if ok := checkUserEqual(user, users[0]); !ok {
 		t.Fatalf(`user is not the same. user:%v users: %v`, user, users[0])
+	} else {
+		logrus.Info("added user matched")
 	}
 }
 
 func TestFindUserById(t *testing.T) {
-	db, close := makeTestUserDB(t)
-	defer close()
+	db := makeTestUserDB(t)
 
 	userDao := MakeUserDAO(db)
 
 	// create new user
 	modelUser := model.User{Name: "Rick", Nickname: "Sanchez"}
 	_, id := userDao.Create(modelUser)
+	logrus.Info("new user created")
 
 	// find user
 	if ok, userFound := userDao.FindById(id); !ok {
 		t.Fatalf(`findById operation failed. ok: %t, userFound: %v`, ok, userFound)
 	} else if !checkUserEqual(modelUser, userFound) {
 		t.Fatalf(`user is not the same. user:%v users: %v`, modelUser, userFound)
+	} else {
+		logrus.Info("matching user found in database")
 	}
 
 	// user with id 1000 shouldn't exist
 	if ok, userFound := userDao.FindById(1000); ok {
 		t.Fatalf(`user with non existent id found. ok:%t users: %v`, ok, userFound)
+	} else {
+		logrus.Info("non existing user not found in database")
 	}
 }
 
 func TestDeleteUserById(t *testing.T) {
-	db, close := makeTestUserDB(t)
-	defer close()
+	db := makeTestUserDB(t)
 
 	userDao := MakeUserDAO(db)
 
 	// create new user
 	modelUser := model.User{Name: "Rick", Nickname: "Sanchez"}
 	ok, id := userDao.Create(modelUser)
+	logrus.Info("new user created")
 
 	// check create user and check if users list increased to 1
 	if !ok {
 		t.Fatalf(`can't create new user.`)
 	} else if ok, users := userDao.FindAll(); !ok || len(users) != 1 {
 		t.Fatalf(`database users doesn't increase. ok: %t, users: %v`, ok, users)
+	} else {
+		logrus.Info("list user in database increased")
 	}
 
 	// delete user and check if users list decreased.
 	if ok := userDao.DeleteById(id); !ok {
 		t.Fatalf(`delete by id operation failed. user id: %d`, id)
-	} else if ok, users := userDao.FindAll(); !ok || len(users) != 0 {
+	} else {
+		logrus.Info("user deleted")
+	}
+
+	if ok, users := userDao.FindAll(); !ok || len(users) != 0 {
 		t.Fatalf(`database users doesn't decrease. ok: %t, users: %v`, ok, users)
+	} else {
+		logrus.Info("list user in database decreased")
 	}
 
 	// delete a nonexistent user id '1000'
 	if ok := userDao.DeleteById(1000); ok {
 		t.Fatalf(`user with nonexistent id deleted.`)
+	} else {
+		logrus.Info("user with non exist id not deleted")
 	}
 }
 
 func TestUpdateUserById(t *testing.T) {
-	db, close := makeTestUserDB(t)
-	defer close()
+	db := makeTestUserDB(t)
 
 	userDao := MakeUserDAO(db)
 
 	// create new user. assume user create is functional and passed previous test
 	modelUser := model.User{Name: "Rick", Nickname: "Sanchez"}
 	_, id := userDao.Create(modelUser)
+	logrus.Info("new user created")
 
 	modelUserToUpdate := model.User{Name: "Rock", Nickname: "Dwayne"}
-	userDao.UpdateById(id, modelUserToUpdate)
+	ok, updatedUser := userDao.UpdateById(id, modelUserToUpdate)
+	if !ok {
+		t.Fatalf(`user update failed`)
+	} else {
+		logrus.Info("user updated")
+	}
+
+	if !checkUserEqual(modelUserToUpdate, updatedUser) {
+		t.Fatalf(`updated user with update values doesn't match`)
+	} else {
+		logrus.Info("updated user with update values match")
+	}
+
+	if ok, userFound := userDao.FindById(id); !ok {
+		t.Fatalf(`user not found`)
+	} else if !checkUserEqual(userFound, updatedUser) {
+		t.Fatalf(`user in database and updated user doesn't match`)
+	} else {
+		logrus.Info("updated user in database match")
+	}
 }
